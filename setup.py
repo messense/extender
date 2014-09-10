511a1712494d65133a0dbabca3d849e2f8c500c2
messense <messense@icloud.com>
2014-09-10 22:06:18 +0800

    Add python wheel package when submit to PyPi

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(readme) as f:
 
 setup(
     name='extender',
-    version='0.1.0',
+    version='0.1.1',
     author='messense',
     author_email='messense@icloud.com',
     url='https://github.com/messense/extender',
